9f002642d9410398d5ac4d904b7b3efc30bead65
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-11-08 10:44:40 -0500

    Author tag for eclipse project and base of REST client

--- RESTClient/src/client/RESTClient.java
+++ RESTClient/src/client/RESTClient.java
@@ -1,10 +1,35 @@
 package client;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
 /**
+ * REST Client that can interrogates a REST web service containing groceries
+ * receipts.
  * 
- * @author TODO
+ * @author Alice Breton, Laora Heintz, Loïc Poncet, Baptiste Rigondaud
  *
  */
 public class RESTClient {
+	
+	/**
+	 * The path to the REST web service
+	 */
+	private String servicePath;
+	
+	/**
+	 * An instance of client to perform requests to the service.
+	 */
+	private Client client;
+	
+	/**
+	 * The REST client is built using the URI to the web service.
+	 * 
+	 * @param servicePath The URI to the web service.
+	 */
+	public RESTClient(String servicePath) {
+		this.servicePath = servicePath;
+		this.client = ClientBuilder.newClient();
+	}
 
 }
