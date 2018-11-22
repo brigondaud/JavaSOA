256bb83a5367ec4da3c6f94746aaf738a4571811
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-11-22 16:29:13 -0500

    changed setup directory

--- docker/tomcat/setup.sh
+++ docker/tomcat/setup.sh
@@ -2,26 +2,26 @@ sudo apt-get update
 sudo apt-get -y install default-jdk
 
 sudo groupadd tomcat
-sudo useradd -s /bin/false -g tomcat -d /home/tomcat tomcat
+sudo useradd -s /bin/false -g tomcat -d ~/tomcat tomcat
 
-mkdir /home/tomcat
+mkdir ~/tomcat
 cd /tmp
 apt-get -y install curl
 curl -O http://mirror.its.dal.ca/apache/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.tar.gz
-tar xzvf apache-tomcat-8*tar.gz -C /home/tomcat --strip-components=1
+tar xzvf apache-tomcat-8*tar.gz -C ~/tomcat --strip-components=1
 
-cd /home/tomcat
-chgrp -R tomcat /home/tomcat
-chmod -R g+r /home/tomcat/conf && chmod g+x /home/tomcat/conf
-chown -R tomcat /home/tomcat/webapps/ /home/tomcat/work/ /home/tomcat/temp/ /home/tomcat/logs/
+cd ~/tomcat
+chgrp -R tomcat ~/tomcat
+chmod -R g+r ~/tomcat/conf && chmod g+x ~/tomcat/conf
+chown -R tomcat ~/tomcat/webapps/ ~/tomcat/work/ ~/tomcat/temp/ ~/tomcat/logs/
 
 jdk=$(update-java-alternatives -l | tr -s ' ' | cut -d' ' -f 3)
 export JAVA_HOME=$jdk/jre
-export CATALINA_PID=/home/tomcat/temp/tomcat.pid
-export CATALINA_PID=/home/tomcat/temp/tomcat.pid
-export CATALINA_HOME=/home/tomcat
-export CATALINA_BASE=/home/tomcat
+export CATALINA_PID=~/tomcat/temp/tomcat.pid
+export CATALINA_PID=~/tomcat/temp/tomcat.pid
+export CATALINA_HOME=~/tomcat
+export CATALINA_BASE=~/tomcat
 export 'CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
 export 'JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
 
-sh /home/tomcat/bin/startup.sh &
+sh ~/tomcat/bin/startup.sh &
