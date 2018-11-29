57aea7b2fe7a34ff03daa622f9a18fa3bdb18c36
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-11-29 13:42:42 -0500

    Spark job updated

--- sparkJob/src/main/java/spark/Job.java
+++ sparkJob/src/main/java/spark/Job.java
@@ -21,7 +21,7 @@ public class Job {
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
       int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
-      int n = 2 * slices;
+      int n = 100 * slices;
       List<Integer> l = new ArrayList<>(n);
       for (int i = 0; i < n; i++) {
         l.add(i);
