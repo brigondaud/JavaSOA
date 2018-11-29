1769f120eb94b11d5da5bd83c3829d41ea7258fd
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-11-29 11:14:07 -0500

    updated simple job

--- sparkJob/src/main/java/spark/Job.java
+++ sparkJob/src/main/java/spark/Job.java
@@ -5,6 +5,8 @@
  */
 package spark;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
@@ -17,8 +19,17 @@ public class Job {
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setAppName("Test Job");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
-        System.out.println("Test job stdout");
-        sc.stop();
+        List<Integer> l = new ArrayList<>(100);
+        for (int i = 0; i < 100; i++) {
+          l.add(i);
+        }
+
+        long count = sc.parallelize(l).filter(i -> {
+          double x = Math.random();
+          double y = Math.random();
+          return x*x + y*y < 1;
+        }).count();
+        System.out.println("Pi is roughly " + 4.0 * count / 100);
     }
     
 }
