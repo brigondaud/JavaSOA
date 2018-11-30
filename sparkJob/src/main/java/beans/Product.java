8b213eded8438708b2f98bac884db390575dbebc
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-11-30 17:07:09 -0500

    MostUsedProduct job

---
+++ sparkJob/src/main/java/beans/Product.java
@@ -0,0 +1,48 @@
+package beans;
+
+/**
+ * A Product bean such as stored as a field of the receipt.
+ * 
+ * @author Baptiste Rigondaud & Loïc Poncet & Alice Breton & Laora Heintz
+ */
+public class Product {
+    
+    /**
+     * The Product's name.
+     */
+    private String name;
+    
+    /**
+     * The product's price.
+     */
+    private int price;
+    
+    /**
+     * The product's quantity.
+     */
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }    
+}
