2e04fa4642c3ea9687648b37b8205b49f2b5876b
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-11-30 13:45:44 -0500

    -y during service setup for install

--- setup/service/setup.sh
+++ setup/service/setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install curl
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt install nodejs
+sudo apt install -y nodejs
 cd setup/service
 npm install
