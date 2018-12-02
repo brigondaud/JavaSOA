74f54f6d5cef30a59a66950b9066cb7cdddcff11
Baptiste Rigondaud <baptiste.rigondaud@grenoble-inp.org>
2018-12-02 21:32:21 +0000

    Made the test-client on UNIX to make it work

--- client/test-client.sh
+++ client/test-client.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]
-  then
-    echo "Usage: ./test-client.sh <Web service IP>"
-    exit 1
+	then
+	echo "Usage: ./test-client.sh <Web service IP>"
+	exit 1
 fi
 
 cd ~/JavaSOA/client
-
 mvn test -DsparkMasterIP="$1"
